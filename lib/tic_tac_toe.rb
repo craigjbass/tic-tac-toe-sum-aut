e4c4db1651c0d423bcd8834ba268ab8f0eece16c
Craig J. Bass <craig@madetech.co.uk>
2019-09-10 11:51:18 +0100

    remove zipping.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,7 +12,7 @@ class TicTacToe
       %i[_ _ _],
       %i[_ _ _]
     ]
-    (@placed_os.zip([:o]*@placed_os.length) + @placed_xs.zip([:x]*@placed_xs.length)).each do |position, piece|
+    (@placed_os + @placed_xs.zip([:x]*@placed_xs.length)).each do |position, piece|
       board[(position-1) / 3][(position-1) % 3] = piece
     end
 
@@ -20,7 +20,7 @@ class TicTacToe
   end
 
   def place_piece(piece, position)
-    @placed_os << position if piece == :o
+    @placed_os << [position, piece] if piece == :o
     @placed_xs << position if piece == :x
   end
 end
